606afe464d8ff48ee3edaa0fb307f60a756802f3
Dan Siegel <me@dansiegel.net>
2017-06-21 11:12:42 -0700

    converting to netstandard

--- build.ps1
+++ build.ps1
@@ -6,22 +6,10 @@ Param(
 
 Write-Host "Starting $Configuration Build"
 
-$outputDir = "./build"
 $solutionFile = "Prism.Plugin.Popups.sln"
-$projects = "Prism.Plugin.Popups.Autofac","Prism.Plugin.Popups.DryIoc","Prism.Plugin.Popups.Ninject","Prism.Plugin.Popups.Unity"
-
-if(!(Test-Path -Path $outputDir )){
-    New-Item -ItemType directory -Path $outputDir
-}
 
 nuget restore $solutionFile
 
 msbuild $solutionFile /t:Clean
 
 msbuild $solutionFile /p:"Configuration=$Configuration"
-
-foreach( $projectName in $projects )
-{
-    $projFile = "./src/$projectName/$projectName.csproj"
-    nuget pack $projFile -properties "Configuration=$Configuration" -outputdirectory $outputDir
-}
